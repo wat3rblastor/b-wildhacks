46abc9262eda2860c9b47bc8a362bbeb83694ee6
Felix Haba <felixhr2000@gmail.com>
2024-04-07 08:29:16 -0500

    Implement signup, login functionality

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 // pages/login.js
 "use client";
 
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 const LoginPage = () => {
@@ -53,12 +54,14 @@ const LoginPage = () => {
           </div>
 
           <div>
+            <Link href={"/customer_tasks"}>
             <button
               type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Sign in
             </button>
+            </Link>
           </div>
         </form>
       </div>
