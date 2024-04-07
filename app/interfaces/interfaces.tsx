8e5dacda1126478afcac2011ac92703b99f55696
Felix Haba <felixhr2000@gmail.com>
2024-04-06 20:52:41 -0500

    task interface

---
+++ app/interfaces/interfaces.tsx
@@ -0,0 +1,13 @@
+interface Task {
+    taskid: number;
+    userid: number; // Assuming it references an existing user
+    title: string;
+    address: string;
+    duration: string; // Represented as a string, but you might want a specific format or even a Date type
+    location: string;
+    description: string;
+    available: boolean;
+    budget: number;
+    providerid: number; // Assuming it references an existing user, can be nullable if not always set
+  }
+  
