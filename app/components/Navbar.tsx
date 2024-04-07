cc86a1d2a2f783bb82892c9077003a6a60bbb694
Bowen Cheng <bowencheng2005@gmail.com>
2024-04-06 21:19:30 -0500

    add signup/login

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,21 @@
+import Link from 'next/link';
+
+
 function NavBar () {
     return (
         <div className='absolute inset-x-0 top-0 h-fit bg-slate-100 flex flex-row justify-between p-5 z-2'>
             <div>Logo</div>
             <div className='flex flex-row gap-4'>
-                <div className='bg-slate-200 p-2 rounded-lg'>Sign Up</div>
-                <div className='bg-slate-200 p-2 rounded-lg'>Log In</div>
+                <div className='bg-slate-200 p-2 rounded-lg'>
+                    <Link href="/signup">
+                        Sign Up
+                    </Link>
+                </div>
+                <div className='bg-slate-200 p-2 rounded-lg'>
+                    <Link href="/login">
+                        Login
+                    </Link>
+                </div>
             </div>
         </div>
     );
